11a8551525be24209e9313468022092dc1b1cd29
Lucas S <lucas.sorribes@gmail.com>
2022-06-01 00:01:12 +0200

    Add c02_e02

---
+++ chapter02_thebasics/c02_e02.rb
@@ -0,0 +1,11 @@
+# Use the modulo operator, division, or a combination of both to take a 4 digit number and find the digit in the: 
+# 1) thousands place 2) hundreds place 3) tens place 4) ones place
+
+puts "1) The digit in the thousands place of 1986 is: #{1986 / 1000}"
+
+puts "2) The digit in the hundreds place of 1986 is: #{1986 / 100 % 10}"
+
+puts "3) The digit in the tens place of 1986 is: #{1986 / 10 % 10}"
+
+puts "4) The digit in the ones place of 1986 is: #{1986 % 10}"
+
