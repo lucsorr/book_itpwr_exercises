26e8ff280de185e227af382ad183be7cacf544f7
Lucas S <lucas.sorribes@gmail.com>
2022-06-01 01:39:16 +0200

    Add c02_e06.rb

---
+++ chapter02_thebasics/c02_e06.rb
@@ -0,0 +1,5 @@
+# Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen.
+
+puts "The square of 7 is #{12**2}"
+puts "The square of 19 is #{19**2}"
+puts "The square of 42 is #{42**2}"
