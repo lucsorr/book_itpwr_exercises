5d30d6daaa23f5b120c1c9da3191fcd9a9b5edd2
Lucas S <lucas.sorribes@gmail.com>
2022-06-01 19:46:02 +0200

    Add c05_e01.rb

---
+++ chapter05_flowcontrol/c05_e01.rb
@@ -0,0 +1,7 @@
+# Write down whether the following expressions return true or false. Then type the expressions into irb to see the results.
+
+(32 * 4) >= 129 # False
+false != !true # False
+true == 4 # False
+false == (847 == '874') # True
+(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false # True
