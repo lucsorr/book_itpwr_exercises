4ef460fb6ab63da7ce5a4bee0e0d4e65aa4b177a
Lucas S <lucas.sorribes@gmail.com>
2022-06-01 13:55:58 +0200

    Add new section (exercise c02_e03) to name.rb

--- chapter03_variables/name.rb
+++ chapter03_variables/name.rb
@@ -3,3 +3,7 @@
 print "Hi! What is your name? "
 user_name = gets.chomp
 puts "Hello, #{user_name}! 🙂"
+
+# Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row. 
+
+10.times {puts user_name}
